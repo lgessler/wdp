331de5c76baf849790d7c4fb445d71599db99ae6
aryamanarora <aryaman.arora2020@gmail.com>
2021-01-20 20:38:05 -0500

    add basic Wiktionary-formatting and uploading functionality

--- src/wdp/models.py
+++ src/wdp/models.py
@@ -3,6 +3,7 @@
 TODO
 """
 import logging
+import pywikibot, re
 
 from wdp import __version__
 
@@ -11,6 +12,14 @@ __copyright__ = "Luke Gessler"
 __license__ = "mit"
 
 _logger = logging.getLogger(__name__)
+
+POS_SHORTCUTS = {
+    'n': 'noun',
+    'v': 'verb',
+    'adj': 'adjective',
+    'prep': 'preposition',
+    'adv': 'adverb'
+}
 
 
 class _DefaultReprMixin:
@@ -37,6 +46,8 @@ class Pronunciation(_DefaultReprMixin):
 
 class Definition(_DefaultReprMixin):
     def __init__(self, definition, part_of_speech=None):
+        if isinstance(definition, str):
+            definition = [definition]
         self.definition = definition
         self.part_of_speech = part_of_speech
 
@@ -46,9 +57,10 @@ class Entry(_DefaultReprMixin):
     TODO
     """
     def __init__(self,
-                 word_form):
+                 word_form, lang_code, lang_name):
         self.word_form = word_form
-
+        self.lang_code = lang_code
+        self.lang_name = lang_name
         self.definitions = []
         self.alternate_forms = []
         self.pronunciations = []
@@ -67,4 +79,53 @@ class Entry(_DefaultReprMixin):
         self.pronunciations.append(
             Pronunciation(pronunciation, notation=notation)
         )
+
+    def export(self):
+        result = f'=={self.lang_name}=='
+
+        if self.alternate_forms:
+            result += '\n\n===Alternative forms===\n'
+            result += '\n'.join([f'* {{{{alter|{self.lang_code}|{x.alternate_form}||{x.description_of_use}}}}}' for x in self.alternate_forms])
+
+        if self.pronunciations:
+            result += '\n\n===Pronunciation===\n* {{IPA|' + self.lang_code + '|'
+            result += '|'.join([x.pronunciation for x in self.pronunciations])
+            result += '}}'
+
+        if self.definitions:
+            for definition in self.definitions:
+                part_of_speech = POS_SHORTCUTS.get(definition.part_of_speech, definition.part_of_speech)
+                if part_of_speech == None:
+                    raise Exception("A part of speech is required.")
+                result += f'\n\n==={part_of_speech.capitalize()}==='
+                result += f'\n{{{{head|{self.lang_code}|{part_of_speech}}}}}\n'
+                for sense in definition.definition:
+                    result += f'\n# {sense}'
+        else:
+            raise Exception("The entry has no definitions. At least one definition is required.")
+        
+        return result
+
+    def upload(self):
+        site = pywikibot.Site(code='en', fam='wiktionary')
+        site.login()
+
+        page = pywikibot.Page(site, self.word_form)
+        order = re.findall(r'(^|\n)==([^=]*?)==[\n]', page.text)
+        print(page.text)
+        print(order)
+        
+        if order:
+            for _, lang in order:
+                if lang == self.lang_name:
+                    print(f'Entry already exists for {self.lang_name} at "{self.word_form}", skipping.')
+                    return
+                elif lang > self.lang_name:
+                    page.text = page.text.replace(f'=={lang}==', f'{self.export()}\n\n----\n\n=={lang}==')
+                    break
+        else:
+            page.text = self.export()
+
+        page.save(f'Automatic import of {self.lang_name} entry with WDP.')
+
 
