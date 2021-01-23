91e26a249b85775a3760fe9781fbf13b675b8d05
Luke Gessler <lukegessler@gmail.com>
2021-01-23 00:51:41 -0500

    improve debug output

--- src/scratch.py
+++ src/scratch.py
@@ -9,6 +9,9 @@ bank_entry.add_pronunciation("beŋk", "IPA")
 bank_entry.add_definition("A place where you put your money", part_of_speech="Noun")
 bank_entry.add_definition("A collection of annoying things, like syntax trees", part_of_speech="Verb")
 bank_entry.add_definition("<Imaginary verbal sense of bank>", part_of_speech="Verb")
+print("Step 1 output:")
+print(bank_entry)
+print()
 
 # other_bank_entry = Entry("bank")
 # other_bank_entry.add_definition("The edges of a river")
@@ -16,6 +19,9 @@ bank_entry.add_definition("<Imaginary verbal sense of bank>", part_of_speech="Ve
 # Step 2: format the entries into a list of 2-tuples: first is the word's form, second is the mwtext for the entry
 entries = [color_entry, bank_entry]
 formatted_entries = format_entries(entries, "en", "English")
+print("Step 1 output:")
+print(formatted_entries[1])
+print()
 
 # Step 3: upload (page_override forces all word forms to be inserted into a debug page)
 upload_formatted_entries(formatted_entries, 'English', page_override='User:AryamanA/test123')
