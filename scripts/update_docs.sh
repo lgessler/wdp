6c2d7f2b454070bdf869c64c124d4c1fca9c834b
Luke Gessler <lukegessler@gmail.com>
2021-03-09 16:33:08 -0500

    clean first

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -3,6 +3,7 @@ if [[ $(git diff --stat) != '' ]]; then
         echo "Commit or discard all changes before updating docs"
         exit 1
 fi
+make -C docs clean
 make -C docs html
 cp -r docs/_build/html tmp
 git checkout gh-pages
