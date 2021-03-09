1c588effeb81db8a1223ddc1c7f3c797c1c4809a
Luke Gessler <lukegessler@gmail.com>
2021-03-09 16:23:58 -0500

    Update docs

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,10 +3,17 @@ if [[ $(git diff --stat) != '' ]]; then
 	echo "Commit or discard all changes before releasing"
 	exit 1
 fi
+# check for version
 if [ $# -eq 0 ]
 then
 	echo "Supply a version (e.g. 0.2.1)"
 	exit 1
+fi
+# check for twine
+if ! command -v twine &> /dev/null
+then
+	echo "twine could not be found (do you need to \`pip install twine\`?)"
+	exit
 fi
 
 rm -rf build/ dist/
