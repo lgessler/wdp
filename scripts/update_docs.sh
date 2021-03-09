77cbcae575aed087721f7dc65f3662750fc05b77
Luke Gessler <lukegessler@gmail.com>
2021-03-09 16:25:21 -0500

    check git status before running

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [[ $(git diff --stat) != '' ]]; then
+        echo "Commit or discard all changes before updating docs"
+        exit 1
+fi
 make -C docs html
 cp -r docs/_build/html tmp
 git checkout gh-pages
