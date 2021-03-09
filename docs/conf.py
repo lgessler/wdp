b42d264f3d36983ed208e03511923f5e67280511
Luke Gessler <lukegessler@gmail.com>
2021-03-09 17:52:07 -0500

    remove alabaster-specific config

--- docs/conf.py
+++ docs/conf.py
@@ -151,8 +151,8 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "sidebar_width": "300px",
-    "page_width": "1200px"
+    #"sidebar_width": "300px",
+    #"page_width": "1200px"
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
